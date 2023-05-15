efccf0aa36b18e000b7bba1ffc0f1bc5f2207e05
Kenji Tjandra <kenjivisit@gmail.com>
2023-05-15 21:29:55 +0700

    first commit

---
+++ src/projectinve/Koneksi.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectinve;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+import java.sql.Statement;
+import java.sql.*;
+
+/**
+ *
+ * @author ktjan
+ */
+public class Koneksi {
+    private static Connection koneksi;
+    static Connection conn;
+    static String sql;
+    static Statement stat;
+    static PreparedStatement pst;
+    static ResultSet rs;
+    
+    public static Connection getKoneksi(){
+        if (koneksi == null){
+            try{
+                String url = "jdbc:mysql://localhost:3306/toko_atk";
+                String user = "root";
+                String password = "";
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                conn = DriverManager.getConnection(url, user, password);
+                JOptionPane.showMessageDialog(null, "Connection Successfully");
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Error");
+            }
+        }
+        return koneksi;
+    }
+}
---
+++ src/projectinve/clsLogin.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectinve;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ktjan
+ */
+public class clsLogin extends Koneksi{
+    String mUsername = "admin";
+    String mPassword = "admin";
+    
+    public void Masuk()
+    {
+        try{
+            sql = "SELECT * FROM login WHERE Username = ? AND Password = ?";
+            pst = conn.prepareStatement(sql);
+            pst.setString(1, mUsername);
+            pst.setString(2, String.valueOf(mPassword));
+            rs = pst.executeQuery();
+            if(rs.next()){
+                FHome home = new FHome();
+                home.setVisible(true);
+                JOptionPane.showMessageDialog(null, "Login berhasil");
+            }else {
+                JOptionPane.showMessageDialog(null, "Login gagal");                                
+            }
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+}
